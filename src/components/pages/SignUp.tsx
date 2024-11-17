7dd5ddc1d98e228d095c3e2720b4b2e5727c435e
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-11-17 20:43:10 +0900

    アカウント作成機能追加

--- src/components/pages/SignUp.tsx
+++ src/components/pages/SignUp.tsx
@@ -3,6 +3,9 @@ import { PasswordInput } from "../ui/password-input";
 import { Button } from "../ui/button";
 import { useNavigate } from "react-router-dom";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { useState } from "react";
+import { User } from "@/domain/User";
+import { createUser } from "@/utils/supabaseFunction";
 
 type Inputs = {
   username: string;
@@ -11,6 +14,9 @@ type Inputs = {
 
 const SignUp = () => {
   const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [userData, setUserData] = useState<User[]>([]);
 
   const {
     register,
@@ -20,22 +26,42 @@ const SignUp = () => {
 
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
-  const onClickSignUpButton = (data) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    if (name === "username") {
+      setUsername(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onClickSignUpButton = () => {
+    const newUserData: User = {
+      username: username,
+      password: password,
+    };
+    const newUserDatas = [...userData, newUserData];
+    setUserData(newUserDatas);
     console.log("クリックしました");
-    console.log(data);
+    createUser(username, password);
     navigate("/Clothes");
+  };
+
+  const onClickBackButton = () => {
+    navigate("/");
   };
 
   return (
     <Box px={10} bg="white" h="vh" position="relative">
       <Box pt={20}>
-        <Text color="red.300" fontSize="3xl" textAlign="center" mb={20}>
+        <Text color="red.300" fontSize="4xl" textAlign="center" mb={20}>
           SIgn Up
         </Text>
         <Box mb={10}>
           <form onSubmit={handleSubmit(onSubmit)}>
             <Input
-              {...register("username", { required: true })}
+              {...register("username", { required: true, onChange: handleChange })}
+              value={username}
               placeholder="Username"
               variant="flushed"
               _placeholder={{ color: "red.300" }}
@@ -49,7 +75,8 @@ const SignUp = () => {
               </Text>
             )}
             <PasswordInput
-              {...register("password", { required: true })}
+              {...register("password", { required: true, onChange: handleChange })}
+              value={password}
               placeholder="password"
               variant="flushed"
               _placeholder={{ color: "red.300" }}
@@ -71,8 +98,23 @@ const SignUp = () => {
             variant="subtle"
             bg="red.300"
             color="white"
+            mb={5}
+            borderRadius="999px"
           >
             Sign Up
+          </Button>
+        </Center>
+        <Center>
+          <Button
+            onClick={onClickBackButton}
+            w={200}
+            variant="outline"
+            bg="white"
+            color="red.300"
+            borderRadius="999px"
+            borderColor="red.300"
+          >
+            Back
           </Button>
         </Center>
       </Box>
