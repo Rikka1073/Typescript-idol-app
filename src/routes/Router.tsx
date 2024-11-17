d0fe0ca7afdbaed413a4ab45d87f67559f06e87a
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-11-17 19:49:59 +0900

    ルーティング設定変更

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -8,7 +8,7 @@ import { Route, Routes } from "react-router-dom";
 const Router = () => {
   return (
     <Routes>
-      <Route path="/login" element={<Login />}></Route>
+      <Route path="/" element={<Login />}></Route>
       <Route path="/SignUp" element={<SignUp />}></Route>
       <Route path="/Clothes" element={<Clothes />}></Route>
       <Route path="/Clothes/:id" element={<Clothe />}></Route>
