95fd50e720944b889884c4660863afc734cfd890
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-11-17 20:43:25 +0900

    ログイン機能追加

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -3,6 +3,7 @@ import { PasswordInput } from "../ui/password-input";
 import { Button } from "../ui/button";
 import { useNavigate } from "react-router-dom";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { getAllUsersData } from "@/utils/supabaseFunction";
 
 type Inputs = {
   username: string;
@@ -20,17 +21,30 @@ const Login = () => {
 
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
-  const onClickLoginButton = (data) => {
-    console.log("クリックしました");
-    console.log(data);
+  const onClickLoginButton = async () => {
+    // const userData = await getAllUsersData();
+    // const userExists = userData.some((user) => user.username === username);
+    // if (userExists) {
+    //   navigate("/Clothes");
+    // } else {
+    //   alert("ユーザーが存在しません");
+    // }
     navigate("/Clothes");
+  };
+
+  const onClickSignUpButton = () => {
+    navigate("/SignUp");
+  };
+
+  const onClickBackButton = () => {
+    navigate("/");
   };
 
   return (
     <>
       <Box px={10} bg="red.300" h="vh" position="relative">
         <Box pt={20}>
-          <Text color="white" fontSize="3xl" textAlign="center" mb={20}>
+          <Text color="white" fontSize="4xl" textAlign="center" mb={20}>
             Login
           </Text>
           <Box mb={10}>
@@ -61,13 +75,27 @@ const Login = () => {
             </form>
           </Box>
           <Center mb={5}>
-            <Button onClick={onClickLoginButton} disabled={!isValid} w={200} variant="subtle">
+            <Button
+              onClick={onClickLoginButton}
+              disabled={!isValid}
+              w={200}
+              variant="subtle"
+              borderRadius="999px"
+            >
               Login
             </Button>
           </Center>
           <Center>
-            <Button w={200} variant="subtle">
-              Sign Up
+            <Button
+              onClick={onClickBackButton}
+              w={200}
+              variant="outline"
+              bg="red.300"
+              color="white"
+              borderRadius="999px"
+              borderColor="white"
+            >
+              Back
             </Button>
           </Center>
         </Box>
