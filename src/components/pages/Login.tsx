bc12f770b881142ef4f9241ebbcfbf48167c7a8a
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-11-17 14:55:35 +0900

    バリデーション機能の追加

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -2,38 +2,66 @@ import { Box, Center, Input, Text } from "@chakra-ui/react";
 import { PasswordInput } from "../ui/password-input";
 import { Button } from "../ui/button";
 import { useNavigate } from "react-router-dom";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type Inputs = {
+  username: string;
+  password: string;
+};
 
 const Login = () => {
   const navigate = useNavigate();
-  const onClickLoginButton = () => {
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<Inputs>({ mode: "onChange" });
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  const onClickLoginButton = (data) => {
     console.log("クリックしました");
+    console.log(data);
     navigate("/Clothes");
   };
 
   return (
     <>
-      <Box px={10} bg="red.400" h="vh">
+      <Box px={10} bg="red.300" h="vh" position="relative">
         <Box pt={20}>
           <Text color="white" fontSize="3xl" textAlign="center" mb={20}>
             Login
           </Text>
           <Box mb={10}>
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
               <Input
+                {...register("username", { required: true })}
                 placeholder="Username"
                 variant="flushed"
                 _placeholder={{ color: "white" }}
                 mb={5}
               />
+              {errors.username && (
+                <Text color="black" mb={2}>
+                  ユーザーネームの入力は必須です
+                </Text>
+              )}
               <PasswordInput
+                {...register("password", { required: true })}
                 placeholder="password"
                 variant="flushed"
                 _placeholder={{ color: "white" }}
               />
+              {errors.password && (
+                <Text color="black" mt={5}>
+                  パスワードの入力は必須です
+                </Text>
+              )}
             </form>
           </Box>
           <Center mb={5}>
-            <Button w={200} variant="subtle" onClick={onClickLoginButton}>
+            <Button onClick={onClickLoginButton} disabled={!isValid} w={200} variant="subtle">
               Login
             </Button>
           </Center>
