a5c94b0aada46b6ba4608aa6345f27501e4b7f08
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-11-17 20:44:08 +0900

    getAllUsersData関数の作成

--- src/utils/supabaseFunction.ts
+++ src/utils/supabaseFunction.ts
@@ -1,5 +1,17 @@
 import { supabase } from "./supabase";
 
+export const getAllUsersData = async () => {
+  const { data, error } = await supabase.from("users_id").select("*");
+
+  if (error) {
+    console.log("Error fetching data with users_id:", error);
+  } else {
+    console.log("Fetched data with users_id:", data);
+  }
+
+  return data;
+};
+
 export const createUser = async (username: string, password: string) => {
   const { data, error } = await supabase
     .from("users_id")
