1416b3bf26a9dc300795f7aeb027693e26e6e15d
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-11-17 20:42:15 +0900

    ルーティング修正

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,5 +1,6 @@
 import Clothe from "@/components/pages/Clothe";
 import Clothes from "@/components/pages/Clothes";
+import Home from "@/components/pages/Home";
 import Login from "@/components/pages/Login";
 import Register from "@/components/pages/Register";
 import SignUp from "@/components/pages/SignUp";
@@ -8,7 +9,8 @@ import { Route, Routes } from "react-router-dom";
 const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<Login />}></Route>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/Login" element={<Login />}></Route>
       <Route path="/SignUp" element={<SignUp />}></Route>
       <Route path="/Clothes" element={<Clothes />}></Route>
       <Route path="/Clothes/:id" element={<Clothe />}></Route>
