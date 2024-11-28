0dfe30c86a008d37cef5faa1f40d5d142c9a6183
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-11-28 23:19:51 +0900

    画像の一覧を表示

--- src/components/pages/Clothes.tsx
+++ src/components/pages/Clothes.tsx
@@ -1,5 +1,48 @@
+import { supabase } from "@/utils/supabase";
+import { Box, Spinner, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+
 const Clothes = () => {
-  return <div>Clothes</div>;
+  const [images, setImages] = useState<string[]>([]);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      const { data, error } = await supabase.storage.from("pictures").list("Clothes");
+      if (data) {
+        console.log(data);
+        const urls = await Promise.all(
+          data.map(async (image) => {
+            const { data } = supabase.storage
+              .from("pictures")
+              .getPublicUrl(`Clothes/${image.name}`);
+            console.log(data.publicUrl);
+            return data.publicUrl;
+          })
+        );
+        setImages(urls);
+      } else {
+        console.error(error);
+      }
+    };
+    fetchImages();
+  }, []);
+
+  return (
+    <Box>
+      {images.length === 0 ? (
+        <Box display="flex" alignItems="center" justifyContent="center" h="100vh">
+          <Box textAlign="center">
+            <Spinner color="purple.400" />
+            <Text color="purple.400" textAlign="center" mt={2}>
+              Loading...
+            </Text>
+          </Box>
+        </Box>
+      ) : (
+        images.map((image, index) => <img key={index} src={image} alt="clothes" />)
+      )}
+    </Box>
+  );
 };
 
 export default Clothes;
