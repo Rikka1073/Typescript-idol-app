2d79eb1472f48221c6e973d3fe269728cc6a7130
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-12-02 22:42:06 +0900

    supabaseの処理切り離し

--- src/components/pages/Clothes.tsx
+++ src/components/pages/Clothes.tsx
@@ -1,61 +1,66 @@
-import { supabase } from "@/utils/supabase";
-import { Box, Image, Spinner, Text } from "@chakra-ui/react";
+import { Box, Image, Link, Spinner, Text } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import Menu from "../Templetes/Menu";
+import { fetchImages } from "@/utils/supabaseFunction";
 
 const Clothes = () => {
-  const [images, setImages] = useState<string[]>([]);
-
+  const [images, setImages] = useState<string[] | undefined>([]);
+  const [error, setError] = useState<string | null>(null);
   useEffect(() => {
-    const fetchImages = async () => {
-      const { data, error } = await supabase.storage.from("pictures").list("Clothes");
-      if (data) {
-        console.log(data);
-        const filteredImages = data.filter((image) => image.name !== ".emptyFolderPlaceholder");
-        const urls = await Promise.all(
-          filteredImages.map(async (image) => {
-            const { data } = supabase.storage
-              .from("pictures")
-              .getPublicUrl(`Clothes/${image.name}`);
-            console.log(data.publicUrl);
-            return data.publicUrl;
-          })
-        );
+    fetchImages()
+      .then((urls) => {
         setImages(urls);
-      } else {
-        console.error(error);
-      }
-    };
-    fetchImages();
+      })
+      .catch((error) => {
+        setError(error);
+        console.log(error);
+      });
   }, []);
 
+  if (error)
+    return (
+      <Text color="red.500" fontWeight="bold" textAlign="center">
+        データが取得できません
+      </Text>
+    );
+
   return (
-    <Box
-      bg="white"
-      px={5}
-      display="flex"
-      flexWrap="wrap"
-      rowGap={8}
-      gap={8}
-      justifyContent="space-between"
-    >
-      {images.length === 0 ? (
-        <Box display="flex" alignItems="center" justifyContent="center" h="100vh" w="full">
-          <Box textAlign="center">
-            <Spinner color="purple.400" />
-            <Text color="purple.400" textAlign="center" mt={2}>
-              Loading...
-            </Text>
-          </Box>
+    <Box>
+      <Box position="fixed" top={0} w="100%" bg="yellow.100" left="0" textAlign="center" h="50px">
+        <Box>
+          <Text>Header</Text>
         </Box>
-      ) : (
-        images.map((image, index) => (
-          <Box bg="purple.100" w="45%" h="30%" rounded="xl">
-            <Image key={index} src={image} alt="clothes" w="80%" margin="auto" />
+      </Box>
+      <Box
+        bg="white"
+        px={5}
+        display="flex"
+        flexWrap="wrap"
+        rowGap={8}
+        gap={8}
+        justifyContent="space-between"
+        mt="65px"
+        overflow="scroll"
+        h="calc(100vh - 50px)"
+      >
+        {!images || images.length === 0 ? (
+          <Box display="flex" alignItems="center" justifyContent="center" h="100vh" w="full">
+            <Box textAlign="center">
+              <Spinner color="purple.400" />
+              <Text color="purple.400" textAlign="center" mt={2}>
+                Loading...
+              </Text>
+            </Box>
           </Box>
-        ))
-      )}
-      <Menu />
+        ) : (
+          images.map((image, index) => (
+            <Box key={index} bg="purple.100" w="45%" h="30%" rounded="xl">
+              <Image src={image} alt="clothes" w="80%" margin="auto" />
+            </Box>
+          ))
+        )}
+        <Menu />
+      </Box>
     </Box>
   );
 };
