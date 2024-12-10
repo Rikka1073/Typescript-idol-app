c6e21cae8a18d690b2b15cff983e5d242a78a0c5
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-12-10 21:30:31 +0900

    回答表示エリア追加

--- src/components/pages/Clothe.tsx
+++ src/components/pages/Clothe.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import Menu from "../Templetes/Menu";
 import { useParams } from "react-router-dom";
 import { supabase } from "@/utils/supabase";
+import Header from "../Templetes/Header";
 
 type ImageType = {
   file_name: string;
@@ -11,7 +12,8 @@ type ImageType = {
 };
 
 const Clothe = () => {
-  const [images, setImages] = useState<ImageType[] | undefined>([]);
+  const [images, setImages] = useState<ImageType | undefined>(undefined);
+  const [answer, setAnswer] = useState<string | null>(false);
   const { id } = useParams();
 
   useEffect(() => {
@@ -20,6 +22,7 @@ const Clothe = () => {
         try {
           const { data } = await supabase.from("clothes").select("*").eq("id", id).single();
           setImages(data);
+          console.log(data);
         } catch (error) {
           console.error("Error fetching image:", error);
         }
@@ -30,21 +33,10 @@ const Clothe = () => {
 
   return (
     <Box>
+      <Header />
       <Text>{id}のページです</Text>
-
-      <Box
-        bg="white"
-        px={5}
-        display="flex"
-        flexWrap="wrap"
-        rowGap={8}
-        gap={8}
-        justifyContent="space-between"
-        mt="65px"
-        overflow="scroll"
-        h="calc(100vh - 50px)"
-      >
-        {!images || images.length === 0 ? (
+      <Box bg="white" px={5} mt="65px">
+        {!images ? (
           <Box display="flex" alignItems="center" justifyContent="center" h="100vh" w="full">
             <Box textAlign="center">
               <Spinner color="purple.400" />
@@ -54,11 +46,29 @@ const Clothe = () => {
             </Box>
           </Box>
         ) : (
-          <Box bg="purple.100" w="45%" h="30%" rounded="xl">
-            <Image src={`${images.file_url}`} alt="clothes" w="80%" margin="auto" />
+          <Box mb={5}>
+            <Image src={`${images.file_url}`} alt="clothes" w="100%" margin="auto" />
           </Box>
         )}
-        <Menu />
+        {!answer ? (
+          <Box
+            bg="purple.100"
+            w="100%"
+            h="200px"
+            rounded="xl"
+            py={5}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Text color="black" textAlign="center" mt={2} fontWeight="bold">
+              回答がありません！
+            </Text>
+          </Box>
+        ) : (
+          <div>a</div>
+        )}
+        <Menu pageId={id} />
       </Box>
     </Box>
   );
