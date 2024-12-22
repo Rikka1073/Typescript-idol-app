f296966dba21eb2a7e20ec71c2bd4d5f2dab9ae6
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2024-12-22 22:08:15 +0900

    css修正

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -3,6 +3,7 @@ import { PasswordInput } from "../ui/password-input";
 import { Button } from "../ui/button";
 import { useNavigate } from "react-router-dom";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { getAllUsersData } from "@/utils/supabaseFunction";
 
 type Inputs = {
   username: string;
@@ -16,19 +17,25 @@ const Login = () => {
     register,
     handleSubmit,
     formState: { errors, isValid },
+    watch,
   } = useForm<Inputs>({ mode: "onChange" });
 
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
+  const username = watch("username");
+  const password = watch("password");
+
   const onClickLoginButton = async () => {
-    // const userData = await getAllUsersData();
-    // const userExists = userData.some((user) => user.username === username);
-    // if (userExists) {
-    //   navigate("/Clothes");
-    // } else {
-    //   alert("ユーザーが存在しません");
-    // }
-    navigate("/Clothes");
+    const userData = await getAllUsersData();
+    const userExists = userData?.some(
+      (user) => user.username === username && user.password === password
+    );
+    if (userExists) {
+      navigate("/Clothes");
+      console.log("ログイン成功");
+    } else {
+      alert("ユーザーが存在しません");
+    }
   };
 
   const onClickBackButton = () => {
@@ -37,13 +44,18 @@ const Login = () => {
 
   return (
     <>
-      <Box px={10} bg="red.300" h="vh" position="relative">
+      <Box
+        px={{ base: "20px", sm: "100px", md: "160px", lg: "200px" }}
+        bg="red.300"
+        h="vh"
+        position="relative"
+      >
         <Box pt={20}>
           <Text color="white" fontSize="4xl" textAlign="center" mb={20}>
             Login
           </Text>
-          <Box mb={10}>
-            <form onSubmit={handleSubmit(onSubmit)}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Box mb={10}>
               <Input
                 {...register("username", { required: true })}
                 placeholder="Username"
@@ -67,32 +79,33 @@ const Login = () => {
                   パスワードの入力は必須です
                 </Text>
               )}
-            </form>
-          </Box>
-          <Center mb={5}>
-            <Button
-              onClick={onClickLoginButton}
-              disabled={!isValid}
-              w={200}
-              variant="subtle"
-              borderRadius="999px"
-            >
-              Login
-            </Button>
-          </Center>
-          <Center>
-            <Button
-              onClick={onClickBackButton}
-              w={200}
-              variant="outline"
-              bg="red.300"
-              color="white"
-              borderRadius="999px"
-              borderColor="white"
-            >
-              Back
-            </Button>
-          </Center>
+            </Box>
+            <Center mb={5}>
+              <Button
+                onClick={onClickLoginButton}
+                disabled={!isValid}
+                w={200}
+                variant="subtle"
+                borderRadius="999px"
+                type="submit"
+              >
+                Login
+              </Button>
+            </Center>
+            <Center>
+              <Button
+                onClick={onClickBackButton}
+                w={200}
+                variant="outline"
+                bg="red.300"
+                color="white"
+                borderRadius="999px"
+                borderColor="white"
+              >
+                Back
+              </Button>
+            </Center>
+          </form>
         </Box>
       </Box>
     </>
--- src/components/pages/SignUp.tsx
+++ src/components/pages/SignUp.tsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { useState } from "react";
 import { User } from "@/domain/User";
-import { createUser } from "@/utils/supabaseFunction";
+import { createUser, getAllUsersData } from "@/utils/supabaseFunction";
 
 type Inputs = {
   username: string;
@@ -14,37 +14,36 @@ type Inputs = {
 
 const SignUp = () => {
   const navigate = useNavigate();
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
   const [userData, setUserData] = useState<User[]>([]);
 
   const {
     register,
     handleSubmit,
     formState: { errors, isValid },
+    watch,
   } = useForm<Inputs>({ mode: "onChange" });
+  const username = watch("username");
+  const password = watch("password");
 
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    if (name === "username") {
-      setUsername(value);
-    } else if (name === "password") {
-      setPassword(value);
+  const onClickSignUpButton = async () => {
+    const userAcount = await getAllUsersData();
+    if (userAcount) {
+      const userExistsAcount = userAcount.some((user) => user.username === username);
+      if (userExistsAcount) {
+        alert("すでに登録されているユーザー名です");
+      } else {
+        const newUserData: User = {
+          username: username,
+          password: password,
+        };
+        const newUserDatas = [...userData, newUserData];
+        setUserData(newUserDatas);
+        createUser(username, password);
+        navigate("/Clothes");
+      }
     }
-  };
-
-  const onClickSignUpButton = () => {
-    const newUserData: User = {
-      username: username,
-      password: password,
-    };
-    const newUserDatas = [...userData, newUserData];
-    setUserData(newUserDatas);
-    console.log("クリックしました");
-    createUser(username, password);
-    navigate("/Clothes");
   };
 
   const onClickBackButton = () => {
@@ -52,15 +51,20 @@ const SignUp = () => {
   };
 
   return (
-    <Box px={10} bg="white" h="vh" position="relative">
+    <Box
+      px={{ base: "20px", sm: "100px", md: "160px", lg: "200px" }}
+      bg="white"
+      h="vh"
+      position="relative"
+    >
       <Box pt={20}>
         <Text color="red.300" fontSize="4xl" textAlign="center" mb={20}>
           SIgn Up
         </Text>
-        <Box mb={10}>
-          <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Box mb={10}>
             <Input
-              {...register("username", { required: true, onChange: handleChange })}
+              {...register("username", { required: true })}
               value={username}
               placeholder="Username"
               variant="flushed"
@@ -75,7 +79,7 @@ const SignUp = () => {
               </Text>
             )}
             <PasswordInput
-              {...register("password", { required: true, onChange: handleChange })}
+              {...register("password", { required: true })}
               value={password}
               placeholder="password"
               variant="flushed"
@@ -88,35 +92,35 @@ const SignUp = () => {
                 パスワードの入力は必須です
               </Text>
             )}
-          </form>
-        </Box>
-        <Center>
-          <Button
-            onClick={onClickSignUpButton}
-            disabled={!isValid}
-            w={200}
-            variant="subtle"
-            bg="red.300"
-            color="white"
-            mb={5}
-            borderRadius="999px"
-          >
-            Sign Up
-          </Button>
-        </Center>
-        <Center>
-          <Button
-            onClick={onClickBackButton}
-            w={200}
-            variant="outline"
-            bg="white"
-            color="red.300"
-            borderRadius="999px"
-            borderColor="red.300"
-          >
-            Back
-          </Button>
-        </Center>
+          </Box>
+          <Center>
+            <Button
+              onClick={onClickSignUpButton}
+              disabled={!isValid}
+              w={200}
+              variant="subtle"
+              bg="red.300"
+              color="white"
+              mb={5}
+              borderRadius="999px"
+            >
+              Sign Up
+            </Button>
+          </Center>
+          <Center>
+            <Button
+              onClick={onClickBackButton}
+              w={200}
+              variant="outline"
+              bg="white"
+              color="red.300"
+              borderRadius="999px"
+              borderColor="red.300"
+            >
+              Back
+            </Button>
+          </Center>
+        </form>
       </Box>
     </Box>
   );
